c7870553c66dd7b634f992e208b91a96d27604c4
joanaquadzie <joanaq03@gmail.com>
2024-09-13 00:42:45 +0000

    started a new project

---
+++ src/app/aboutmandem/page.tsx
@@ -0,0 +1,4 @@
+export default function Aboutus () {
+return (<h1>The About Page </h1>)
+}
+    
---
+++ src/app/aboutmandem/post/page.tsx
@@ -0,0 +1,3 @@
+export default function Post () {
+    return (<h1>this will be rendered at /aboutmandem/post</h1>)
+}
